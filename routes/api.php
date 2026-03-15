7738d66015212fa319aab5560ecf5d31a14dc96f
shyakabriella <shyakabriella@gmail.com>
2026-03-15 11:36:05 +0200

    reset

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RegisterController;
 
 Route::controller(RegisterController::class)->group(function () {
-    Route::get('roles', 'roles');
-    Route::get('users', 'users');
-
-    Route::post('register', 'register');
     Route::post('login', 'login');
     Route::post('forgot-password', 'forgotPassword');
     Route::post('reset-password', 'resetPassword');
 });
 
-Route::middleware('auth:sanctum')->group(function () {
-    //
+Route::middleware('auth:sanctum')->controller(RegisterController::class)->group(function () {
+    Route::get('roles', 'roles');
+    Route::get('users', 'users');
+    Route::post('register', 'register');
 });
