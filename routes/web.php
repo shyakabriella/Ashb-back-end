8fc830360aa2842a37f15fafd845d7e229c05d4d
shyakabriella <lindabriella@gmail.com>
2026-07-19 16:45:24 +0200

    Add invoice payment redirect route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Public invoice payment redirect
+|--------------------------------------------------------------------------
+|
+| Redirect invoice payment links opened through the API domain to the
+| Next.js frontend payment page.
+|
+*/
+
+\Illuminate\Support\Facades\Route::get(
+    '/invoices/{invoice}/pay',
+    function (string $invoice) {
+        return redirect()->away(
+            'https://www.d.ashbhub.com/invoices/'
+            . $invoice
+            . '/pay'
+        );
+    }
+)
+    ->whereNumber('invoice')
+    ->name('invoices.pay.redirect');
+
